f4572b44ca65fec9df509b6050acb4a49bb73639
Kyle Brandon <kylebrandon17@gmail.com>
2021-07-01 18:51:17 -0700

    golf course owner class added

---
+++ src/main/java/com/example/CourseOwner.java
@@ -0,0 +1,141 @@
+package com.example;
+
+import java.awt.*;
+
+public class CourseOwner {
+
+    private String username;
+    private String password;
+    private String fname;
+    private String lname;
+    private String email;
+    private String address;
+    private String phoneNumber;
+    private String website;
+    private String numHoles;
+    private String yardage;
+    private String courseName;
+    private Image courseLogo;
+    private String directionsToCourse;
+    private String description;
+    private String weekdayRates;
+    private String weekendRates;
+
+
+    public String getUsername() {
+        return this.username;
+    }
+    public void setUsername(String f) {
+        this.username = f;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+    public void setPassword(String f) {
+        this.password = f;
+    }
+
+
+    public String getFname() {
+        return this.fname;
+    }
+    public void setFname(String f) {
+        this.fname = f;
+    }
+
+
+    public String getLname() {
+        return this.lname;
+    }
+    public void setLname(String f) {
+        this.lname = f;
+    }
+
+
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String f) {
+        this.email = f;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+    public void setAddress(String f) {
+        this.address = f;
+    }
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    public void setPhoneNumber(String f) {
+        this.phoneNumber = f;
+    }
+
+    public String getWebsite() {
+        return this.website;
+    }
+    public void setWebsite(String f) {
+        this.website = f;
+    }
+
+    public String getNumHoles() {
+        return this.numHoles;
+    }
+    public void setNumHoles(String f) {
+        this.numHoles = f;
+    }
+
+    public String getYardage() {
+        return this.yardage;
+    }
+    public void setYardage(String f) {
+        this.yardage = f;
+    }
+
+    public String getCourseName() {
+        return this.courseName;
+    }
+    public void setCourseName(String f) {
+        this.courseName = f;
+    }
+
+    public Image getCourseLogo() {
+        return this.courseLogo;
+    }
+    public void setCourseLogo(Image f) {
+        this.courseLogo = f;
+    }
+
+    public String getDirectionsToCourse() {
+        return this.directionsToCourse;
+    }
+    public void setDirectionsToCourse(String f) {
+        this.directionsToCourse = f;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+    public void setDescription(String f) {
+        this.description = f;
+    }
+
+    public String getWeekdayRates() {
+        return this.weekdayRates;
+    }
+    public void setWeekdayRates(String f) {
+        this.weekdayRates = f;
+    }
+
+    public String getWeekendRates() {
+        return this.weekendRates;
+    }
+    public void setWeekendRates(String f) {
+        this.weekendRates = f;
+    }
+
+
+}
