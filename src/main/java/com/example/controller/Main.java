552e457d348ad56aeced93659ee0282b7bb6576f
jeyan <justin_yan_2@sfu.ca>
2021-07-21 17:16:41 -0700

    readded changes

--- src/main/java/com/example/controller/Main.java
+++ src/main/java/com/example/controller/Main.java
@@ -1904,9 +1904,20 @@ public void userInsertScorecard(Connection connection, String username, Scorecar
   )
   public String clearUserDB(Map<String, Object> model){
     try (Connection connection = dataSource.getConnection()){
-      Statement stmt = connection.createStatement();
-      String sql = "DROP TABLE users";
-      stmt.executeUpdate(sql);
+      Statement stmtUser = connection.createStatement();
+      Statement stmtOwner = connection.createStatement();
+
+      ResultSet deleteOwners = stmtUser.executeQuery("SELECT * FROM users");          // Delete owner accounts as well
+      while(deleteOwners.next()){
+        String checkPrioirty = deleteOwners.getString("priority");
+        if (checkPrioirty.equals(priorities[1])){
+          String userName = deleteOwners.getString("username");
+          stmtOwner.executeUpdate("DELETE FROM owners WHERE username='"+userName+"'");
+        }
+      }
+
+      stmtUser.executeUpdate("DROP TABLE users");
+
 
       return "redirect:/tee-rific/admin/users";
     } catch (Exception e) {
@@ -1973,7 +1984,7 @@ public void userInsertScorecard(Connection connection, String username, Scorecar
       }
 
       model.put("tournamentList",output);
-      return "Tournaments/listOfTournaments";
+      return "Admin/listOfTournaments";
     } catch (Exception e) {
       model.put("message", e.getMessage());
       return "LandingPages/error";
@@ -2068,9 +2079,16 @@ public void userInsertScorecard(Connection connection, String username, Scorecar
   )
   public String clearOwnerDB(Map<String, Object> model){
     try (Connection connection = dataSource.getConnection()){
-      Statement stmt = connection.createStatement();
-      String sql = "DROP TABLE users";
-      stmt.executeUpdate(sql);
+      Statement stmtOwner = connection.createStatement();
+      Statement stmtUser = connection.createStatement();
+      ResultSet deleteOwners = stmtOwner.executeQuery("SELECT * FROM owners");
+      while(deleteOwners.next()){
+        String ownerName = deleteOwners.getString("username");
+        stmtUser.executeUpdate("DELETE FROM users WHERE username='"+ownerName+"'");
+      }
+
+      stmtOwner.executeUpdate("DROP TABLE owners");
+
 
       return "redirect:/tee-rific/admin/owners";
     } catch (Exception e) {
