7bbdd39d1960a170498cff72aae0f31c333be98a
ZachSyv <59035093+ZachSyv@users.noreply.github.com>
2021-07-24 03:31:51 -0700

    working on implimenting weather forcast

--- src/main/resources/static/js/weatherAPI.js
+++ src/main/resources/static/js/weatherAPI.js
@@ -1,18 +1,30 @@
 
 //change to http://api.openweathermap.org/data/2.5/weather?q=" + golfCourseCity + "&units=metric&appid=96bf787bdb96400f9a642360f1e901d7
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=vancouver&units=metric&appid=96bf787bdb96400f9a642360f1e901d7", function(data)
+document.getElementById("today6").onclick = function () {getWeatherData()};
+
+function getWeatherData()
 {
-    console.log(data);
-    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=vancouver&units=metric&appid=96bf787bdb96400f9a642360f1e901d7", function(data)
+    {
+        console.log(data); //take out
+        var icon = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
 
-    var temp = Math.floor(data.main.temp);
-    var weather = data.weather[0].main;
-    var wind = data.wind.speed;
+        var temp = Math.floor(data.list[0].main.temp);
+        var weather = data.list[0].weather[0].main; 
+        var weather_desc = data.list[0].weather[0].description;
+        var wind = data.list[0].wind.speed;
 
-    $('.icon').attr('src',icon);
-    $('.weather').append(weather);
-    $('.temp').append(temp);
-    $('.wind').append(wind);
+        $('.icon').attr('src',icon);
+        $('.weather').append(weather);
+        $('.temp').append(temp);
+        $('.wind').append(wind);
+    }
+    );
 }
-);
+
+
+// $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=vancouver&appid=96bf787bdb96400f9a642360f1e901d7")
+// {
+    
+// }
