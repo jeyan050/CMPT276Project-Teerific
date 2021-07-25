bc26dbbb248595484f89f774e232e2b02331f3e3
ZachSyv <59035093+ZachSyv@users.noreply.github.com>
2021-07-25 03:15:06 -0700

    working on implimenting weather forcast

--- src/main/resources/static/js/weatherAPI.js
+++ src/main/resources/static/js/weatherAPI.js
@@ -1,19 +1,42 @@
 
 //change to http://api.openweathermap.org/data/2.5/weather?q=" + golfCourseCity + "&units=metric&appid=96bf787bdb96400f9a642360f1e901d7
 
-document.getElementById("today6").onclick = function () {getWeatherData()};
-
-function getWeatherData()
+window.addEventListener("load", function()
 {
     $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=vancouver&units=metric&appid=96bf787bdb96400f9a642360f1e901d7", function(data)
     {
-        console.log(data); //take out
-        var icon = "http://openweathermap.org/img/w/" + data.list[0].weather[0].icon + ".png";
+        var info_date = new Date(((data.list[0].dt) +  data.city.timezone)*1000);
+        document.getElementById("date1").value = info_date.toDateString();
+        document.getElementById("date2").value = info_date.toDateString();
+        document.getElementById("date3").value = info_date.toDateString();
+        document.getElementById("date4").value = info_date.toDateString();
+        document.getElementById("date5").value = info_date.toDateString();
+        
+        info_date.getHours();
+    }
+    );
+});
 
-        var temp = Math.floor(data.list[0].main.temp);
-        var weather = data.list[0].weather[0].main; 
-        var weather_desc = data.list[0].weather[0].description;
-        var wind = data.list[0].wind.speed;
+
+document.getElementById("today6").onclick = function () {getWeatherData(0)};
+document.getElementById("today9").onclick = function () {getWeatherData(1)};
+document.getElementById("today12").onclick = function () {getWeatherData(2)};
+document.getElementById("today15").onclick = function () {getWeatherData(3)};
+
+function getWeatherData(i)
+{
+    $.getJSON("http://api.openweathermap.org/data/2.5/forecast?q=vancouver&units=metric&appid=96bf787bdb96400f9a642360f1e901d7", function(data)
+    {
+        var info_date = new Date(((data.list[i].dt) +  data.city.timezone)*1000);
+        info_date.getHours();
+
+
+        var icon = "http://openweathermap.org/img/w/" + data.list[i].weather[i].icon + ".png";
+
+        var temp = Math.floor(data.list[i].main.temp);
+        var weather = data.list[i].weather[i].main; 
+        var weather_desc = data.list[i].weather[i].description;
+        var wind = data.list[i].wind.speed;
 
         $('.icon').attr('src',icon);
         $('.weather').append(weather);
