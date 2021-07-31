bcbde4f969e397ae5f4d1531e24f9a8d23cf65c1
Mike Kreutz <mike@Mikes-SFU-MacBook-Pro.local>
2021-07-31 01:01:02 -0700

    fixed conflicting JS file button timeouts

--- src/main/resources/static/js/nuke.js
+++ src/main/resources/static/js/nuke.js
@@ -1,12 +1,34 @@
 var nukeSound = new Audio("https://www.myinstants.com/media/sounds/tactical-nuke.mp3");
-// var timeoutLength = 12000; // in milliseconds (perfect timing for above mp3)
+var golfSwingSound = new Audio("https://ringtons.s3.eu-west-2.amazonaws.com/th5s43yk.mp3");
+var timeoutLength = 500; // in milliseconds (perfect timing for golf swing mp3)
+    
+//submits the form associated with the button
+function submitForm(target){
+    target.parentElement.submit();
+}
+
+//adds event listeners to the homePageButtons
+var numberOfButtons = document.querySelectorAll(".homePageButton").length;
+for(var i = 0; i < numberOfButtons; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", function(event){
+        currentButtonClicked = event.target;
+        
+        event.preventDefault();
+        golfSwingSound.play();
+        
+        setTimeout(submitForm, timeoutLength, event.target);
+    });
+}
 
 
+//gets the next number in the countdown and changes the onscreen text
 function nextNum(count){
     document.querySelectorAll("#countdown")[0].innerText = count;
     console.log(count);
-}
+}//nextNum()
 
+
+//starts the countdown for the onscreen text
 function startCountdown(){
     var count = 4;
 
@@ -16,15 +38,20 @@ function startCountdown(){
             count--;
         }
     }, 1000);
-}
+}//startCountdown()
 
+
+//clear countdownt text
 function clearCountdown(event){
     document.querySelectorAll("#countdown")[0].style.display = "none";
-
+    event.target.parentElement.submit()
 }
 
+//event listener for nuke button
 document.querySelectorAll("#nukeButton")[0].addEventListener("click", function(event){
     //activate countdown
+
+    event.preventDefault();
 
     document.querySelectorAll(".notActive")[0].style.display = "initial";
     document.querySelectorAll(".active")[0].style.display = "none";
@@ -44,5 +71,6 @@ document.querySelectorAll("#nukeButton")[0].addEventListener("click", function(e
     startCountdown();
     setTimeout(clearCountdown, 12000, event);
 })
+
 
 document.querySelectorAll(".notActive")[0].style.display = "none";
