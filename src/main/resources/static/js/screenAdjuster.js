beafa3b1eb64340b5f2ada5627c03651a1d7e81c
Mike Kreutz <mike@Mikes-SFU-MacBook-Pro.local>
2021-08-04 13:15:23 -0700

    another mobile test

--- src/main/resources/static/js/screenAdjuster.js
+++ src/main/resources/static/js/screenAdjuster.js
@@ -1,17 +1,31 @@
+var wideScreenTableRemoved = 0;
+var narrowScreenTableRemoved = 0;
+
+
 //checks the screen side, ensures correct table is used before other functions are run for set up
-if(screen.width < 1025){
-    document.querySelectorAll(".wideScreenTable")[0].remove();
-}else{
-    document.querySelectorAll(".narrowScreenTable")[0].remove();
+function removeTable(){
+    if(window.screen.width < 1025){
+        wideScreenTableRemoved = 1;
+        document.querySelectorAll(".wideScreenTable")[0].remove();
+    }else{
+        narrowScreenTableRemoved = 1;
+        document.querySelectorAll(".narrowScreenTable")[0].remove();
+    }
 }
 
 //adds event listener for resizing the window and reloads so appropriate sized table is used
-var previousScreenWidth = screen.width;
-window.addEventListener('resize', function(event) {
-    //will only reload if the width of the screen changes
-    if(this.screen.width !== previousScreenWidth){
-        this.alert("previous Screen width: " + previousScreenWidth + ", currentScreenWidth: " + this.screen.width);
-        previousScreenWidth = this.screen.width;
-        // window.location.reload();
-    }
-});
+function addScreenEventListener(){
+    window.addEventListener('resize', function(event) {
+        removeTable();
+        
+        //will only reload if both tables have been removed
+        if(wideScreenTableRemoved == 1 && narrowScreenTableRemoved == 1){
+            window.location.reload();
+        }
+    });
+}
+
+removeTable();              //removes one of the tables on startup
+addScreenEventListener();
+
+
